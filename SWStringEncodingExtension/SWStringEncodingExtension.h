b48fff4f26cc04d06075125ecf8d8b3d34bcae47
timer <timer_sevenwang@163.com>
2019-08-15 16:59:06 +0800

    Initial Commit

---
+++ SWStringEncodingExtension/SWStringEncodingExtension.h
@@ -0,0 +1,19 @@
+//
+//  SWStringEncodingExtension.h
+//  SWStringEncodingExtension
+//
+//  Created by SSD on 2019/8/15.
+//  Copyright © 2019 SSD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SWStringEncodingExtension.
+FOUNDATION_EXPORT double SWStringEncodingExtensionVersionNumber;
+
+//! Project version string for SWStringEncodingExtension.
+FOUNDATION_EXPORT const unsigned char SWStringEncodingExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWStringEncodingExtension/PublicHeader.h>
+
+
